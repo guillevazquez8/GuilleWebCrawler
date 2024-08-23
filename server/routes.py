1bba1651e02dc53356189471ac995976e8bc8591
guillevazquez8 <guillevazquez8@gmail.com>
2024-08-23 13:39:34 +0200

    added endpoint to filter entries with more than 5 words ordered by number of comments

--- server/routes.py
+++ server/routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, abort, make_response, jsonify
 from server.model import News
 from server.init_db import init_db
+from sqlalchemy import func, desc
 
 
 app_web_crawler = Blueprint('guille_web_crawler_v1_0_bp', __name__)
@@ -12,8 +13,19 @@ def get_all_news():
     all_news_json = [new.to_dict() for new in all_news]
     return make_response(all_news_json)
 
+
 @app_web_crawler.get("/refresh")
 def refresh_first_30_news():
     News.delete_all()
     entries = init_db()
     return make_response(entries)
+
+
+@app_web_crawler.get("/more_5_words")
+def get_news_with_more_than_5_words_in_title():
+    all_news_ordered = News.query.order_by(desc(News.number_of_comments)).all()
+    news = [new for new in all_news_ordered if len(new.title.split()) > 5]
+    news_json = [new.to_dict() for new in news]
+    return make_response(news_json)
+
+#@app_web_crawler.get("/less_5_words"))
--- tests/test_news.py
+++ tests/test_news.py
@@ -1,3 +1,4 @@
+import sys
 
 
 class TestNews:
@@ -9,3 +10,14 @@ class TestNews:
     def test_refresh(self, client):
         response = client.get("/refresh")
         assert len(response.json) == 30
+
+    def test_more_5_words(self, client):
+        response = client.get("/more_5_words")
+        assert response.status_code == 200
+        number_of_comments = sys.maxsize
+        for entry in response.json:
+            assert len(entry['title'].split()) > 5
+            assert entry['number_of_comments'] <= number_of_comments
+            number_of_comments = entry['number_of_comments']
+
+
